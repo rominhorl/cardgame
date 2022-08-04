3cf63e70c34870d6cd474608b8f5a137fee0f249
rominhorl <66178772+rominhorl@users.noreply.github.com>
2022-08-04 06:15:57 -0300

    Worked on some card RNG

---
+++ CardGame/CardDatabase.gd
@@ -0,0 +1,11 @@
+extends Node
+
+const CardList = {
+
+	"Capivara" : {"Biome": "Pantanal", "Type": "Consumidor1", "Name": "Capivara"},
+	"Cajueiro" : {"Biome": "Caatinga", "Type": "Produtor", "Name": "Cajueiro"},
+	"Periquito" : {"Biome": "Caatinga", "Type": "Consumidor1", "Name": "Periquito"},
+	"Teiu" : {"Biome": "Amazonia", "Type": "Consumidor1", "Name": "Teiu"},
+	"Microbio" : {"Biome": "Atlantica", "Type": "Decompositor", "Name": "Microbio"},
+
+}
--- CardGame/Scenes/Deck.gd
+++ CardGame/Scenes/Deck.gd
@@ -2,22 +2,33 @@ extends Control
 
 var MouseHovering = false
 const Card = preload("res://Scenes/Card.tscn")
+const CardList = preload("res://CardDatabase.gd")
 
 
 func _physics_process(delta):
 	
 	if MouseHovering and Input.is_action_just_pressed("LeftClick"):
-		var NewCard = Card.instance()
-		NewCard.position = Vector2(get_viewport().size.x/2,get_viewport().size.y/2)
-		$Hand.add_child(NewCard)
-		print("clicou")
+		draw_a_card(1)
+#		
 		
 		
 func _on_DeckArea_mouse_entered():
 	MouseHovering = true
-	print("entrou")
 
 
 func _on_DeckArea_mouse_exited():
 	MouseHovering = false
-	print("saiu")
+
+
+func draw_a_card(quantity):
+	var CardRandom = randi()%5
+	var AuxiliaryList = CardList.CardList.keys()
+	var WhichCard = CardList.CardList.get(AuxiliaryList[CardRandom])
+	print(WhichCard)
+	var NewCard = Card.instance()
+	NewCard.get_node("FrontBg").texture = load(str("res://card shapes/",WhichCard.get("Biome"),".png"))
+	NewCard.get_node("Type").texture = load(str("res://Assets/type icons/",WhichCard.get("Type"),".png"))
+#	NewCard.get_node("Type").scale = 0.5
+	NewCard.position = Vector2(get_viewport().size.x/2,get_viewport().size.y/2)
+	$Hand.add_child(NewCard)
+	
