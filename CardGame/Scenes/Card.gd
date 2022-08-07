c20e594cec4c59fbd756163641a2a457515a852f
rominhorl <66178772+rominhorl@users.noreply.github.com>
2022-08-07 10:16:50 -0300

    Click on topmost card and drag to cast card
    We had this issue where when you there were two or more cards overlaping each other and you clicked to pick one you ended up pick all of them. Now this is fixed. Also to fix that we had to change te pick-card mode from just click to click and drag

--- CardGame/Scenes/Card.gd
+++ CardGame/Scenes/Card.gd
@@ -12,21 +12,27 @@ func _physics_process(delta):
 	MousePosition = get_viewport().get_mouse_position()
 	CardPosition = position
 	
-	if MouseHovering and Input.is_action_just_pressed("LeftClick"):
-		if CardFollowingMouse:
-			CardFollowingMouse = false
-		else:
-			CardFollowingMouse = true
+	if MouseHovering and Input.is_action_just_pressed("LeftClick") and IsOnTop():
+		CardFollowingMouse = true
+	if CardFollowingMouse == true and Input.is_action_just_released("LeftClick"):
+		CardFollowingMouse = false
 
 	
 	if CardFollowingMouse == true:
 		move_and_slide(((MousePosition - CardPosition)) * speed * delta)
 		
-	
+
+func IsOnTop():
+	for i in get_tree().get_nodes_in_group("Hovered"):
+		if i.get_index() > get_index():
+			return false
+	return true
 
 func _on_CardArea_mouse_entered():
+	add_to_group("Hovered")
 	MouseHovering = true
 	
 func _on_CardArea_mouse_exited():
+	remove_from_group("Hovered")
 	MouseHovering = false
 	
