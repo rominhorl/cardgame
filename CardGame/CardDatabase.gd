386c801e42d8b1e7c9a05097a2f45d8c898d6a0b
Pedro Reis Lima <gaffetwo@gmail.com>
2022-08-08 18:40:43 -0300

    Added all animal in dict and added background

--- CardGame/CardDatabase.gd
+++ CardGame/CardDatabase.gd
@@ -6,22 +6,22 @@ const CardList = {
 	"Tucano" : {"Biome": "Amazônia", "Type": "Consumidor1", "Name": "Tucano"},
 	"Onça Pintada" : {"Biome": "Amazônia", "Type": "Consumidor2", "Name": "Onça Pintada"},
 	"Fungo A" : {"Biome": "Amazônia", "Type": "Decompositor", "Name": "Fungo"},
-	
+
 	# Caatinga
 	"Cajueiro" : {"Biome": "Caatinga", "Type": "Produtor", "Name": "Cajueiro"},
-	"Periquito" : {"Biome": "Amazônia", "Type": "Consumidor1", "Name": "Periquito"},
-	"Teiu" : {"Biome": "Amazônia", "Type": "Consumidor2", "Name": "Teiu"},
-	"Fungo C" : {"Biome": "Amazônia", "Type": "Decompositor", "Name": "Fungo"},
+	"Periquito" : {"Biome": "Caatinga", "Type": "Consumidor1", "Name": "Periquito"},
+	"Teiu" : {"Biome": "Caatinga", "Type": "Consumidor2", "Name": "Teiu"},
+	"Fungo C" : {"Biome": "Caatinga", "Type": "Decompositor", "Name": "Fungo"},
 	
 	# Mata Atlântica
-	"Bananeira" : {"Biome": "Amazônia", "Type": "Produtor", "Name": "Bananeira"},
-	"Mico Leão Dourado" : {"Biome": "Amazônia", "Type": "Consumidor1", "Name": "Mico Leão Dourado"},
-	"Gavião" : {"Biome": "Amazônia", "Type": "Consumidor2", "Name": "Gavião"},
-	"Fungo M" : {"Biome": "Amazônia", "Type": "Decompositor", "Name": "Fungo"},
+	"Bananeira" : {"Biome": "Mata Atlântica", "Type": "Produtor", "Name": "Bananeira"},
+	"Mico Leão Dourado" : {"Biome": "Mata Atlântica", "Type": "Consumidor1", "Name": "Mico Leão Dourado"},
+	"Gavião" : {"Biome": "Mata Atlântica", "Type": "Consumidor2", "Name": "Gavião"},
+	"Fungo M" : {"Biome": "Mata Atlântica", "Type": "Decompositor", "Name": "Fungo"},
 
 	# Pantanal
-	"Pequi" : {"Biome": "Amazônia", "Type": "Produtor", "Name": "Pequi"},
-	"Capivara" : {"Biome": "Amazônia", "Type": "Consumidor1", "Name": "Capivara"},
-	"Jacaré" : {"Biome": "Amazônia", "Type": "Consumidor2", "Name": "Jacaré"},
-	"Fungo P" : {"Biome": "Amazônia", "Type": "Decompositor", "Name": "Fungo"},
+	"Pequi" : {"Biome": "Pantanal", "Type": "Produtor", "Name": "Pequi"},
+	"Capivara" : {"Biome": "Pantanal", "Type": "Consumidor1", "Name": "Capivara"},
+	"Jacaré" : {"Biome": "Pantanal", "Type": "Consumidor2", "Name": "Jacaré"},
+	"Fungo P" : {"Biome": "Pantanal", "Type": "Decompositor", "Name": "Fungo"},
 }
